6ee899760f78f65c9cecd207238b926142abd559
Ivan Borba <ivan.jrnicolau@gmail.com>
2025-02-24 18:15:38 -0300

    feat: initial commit

---
+++ src/main/java/com/project/coopera_voto/CooperaVotoApplication.java
@@ -0,0 +1,13 @@
+package com.project.coopera_voto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperaVotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperaVotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/coopera_voto/CooperaVotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coopera_voto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperaVotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
